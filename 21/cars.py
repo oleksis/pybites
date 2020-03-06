075d1f3420ca00eace133411e76db0dde57755cf
Oleksis Fraga Menéndez <44526468+oleksis@users.noreply.github.com>
2020-03-06 03:05:52 -0500

    submission Bite 21 @ codechalleng.es

---
+++ 21/cars.py
@@ -0,0 +1,42 @@
+cars = {
+    'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
+    'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
+    'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
+    'Honda': ['Civic', 'Accord', 'Odyssey', 'Jazz'],
+    'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
+}
+
+
+def get_all_jeeps(cars=cars):
+    """return a comma  + space (', ') separated string of jeep models
+       (original order)"""
+    jeeps = cars.get("Jeep", None)
+    return ", ".join(j for j in jeeps)
+
+
+def get_first_model_each_manufacturer(cars=cars):
+    """return a list of matching models (original ordering)"""
+    return [model[0]
+            for model in cars.values()
+            if len(model) != 0
+           ]
+
+
+def get_all_matching_models(cars=cars, grep='trail'):
+    """return a list of all models containing the case insensitive
+       'grep' string which defaults to 'trail' for this exercise,
+       sort the resulting sequence alphabetically"""
+    models = [model
+              for models in cars.values()
+              for model in models
+              if model.lower().find(grep.lower()) != -1]
+    return sorted(models)
+
+
+def sort_car_models(cars=cars):
+    """return a copy of the cars dict with the car models (values)
+       sorted alphabetically"""
+    car_models = dict()
+    for k, models in cars.items():
+        car_models[k] = sorted(models)
+    return car_models
